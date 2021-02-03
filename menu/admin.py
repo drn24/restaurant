cf90df6c6e17a0190fc7ade48c4b2bf266bb0f0b
Darren Nelson <drn24@yahoo.com>
2021-02-03 11:32:05 -0700

    Created categories and items

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from menu.models import *
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Item)
+
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    ordering = models.IntegerField(default=0, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=60)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    desc = models.TextField(max_length=256, default="", blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    ordering = models.IntegerField(default=0, blank=True)
+
+    def __str__(self):
+        return f"{self.name}: {self.category}"
+
